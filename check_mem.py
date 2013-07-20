f0e53e69aba5234798449118104fbca2f0bcf435
Guixing Bai <khsing.cn@gmail.com>
2013-07-20 17:20:22 +0800

    add unit convert function, so that we would accept option include unit, e.g. 12g

--- check_mem.py
+++ check_mem.py
@@ -11,9 +11,20 @@ UNKNOWN=3
 
 def opt():
     parser = OptionParser(usage="usage: %prog -w WARNING -c CRITICAL")
-    parser.add_option("-c", default=100, action="store", type="int", dest="critical")
-    parser.add_option("-w", default=200, action="store", type="int", dest="warning")
+    parser.add_option("-c", default='100M', action="store", type="string", dest="critical")
+    parser.add_option("-w", default='200M', action="store", type="string", dest="warning")
     return parser.parse_args()
+
+
+def convertUnit(s):
+    unit = {'g':2**30,'m':2**20,'k':2**10,'t':2**40,'b':1}
+    s = s.lower()
+    lastchar = s[-1]
+    num = int(s[:-1])
+    if lastchar in unit:
+        return num*unit[lastchar]
+    else:
+        return int(s)
 
 
 def getFreeMemory():
@@ -21,12 +32,12 @@ def getFreeMemory():
         for line in fd.readlines():
             if line.startswith('MemFree'):
                  k, v, u = line.split()
-                 return int(v)/1024
+                 return int(v)*1024
                        
 def main():
     opts, args = opt()
-    w = opts.warning
-    c = opts.critical
+    w = convertUnit(opts.warning)
+    c = convertUnit(opts.critical)
     free_mem = getFreeMemory()
     if w >= free_mem > c:
         print "WARNING, free:%s MB" % free_mem
